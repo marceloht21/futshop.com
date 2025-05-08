5806cf6cb08a566b18ac7db6472c3b26978e22ec
marceloht21 <futshopnlmx@gmail.com>
2025-05-08 16:05:07 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const cartCount = document.getElementById('cart-count');
+let count = 0;
+
+const addToCartButtons = document.querySelectorAll('.add-to-cart');
+
+addToCartButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        count++;
+        cartCount.textContent = count;
+    });
+});
